f08c5769e0e3b6e94cf0e980c879c9b5698c8085
Anhkiet2401 <kietb1910537@student.ctu.edu.vn>
2023-11-24 06:58:35 +0700

    client

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,53 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+           <h2>Cần Thơ vivu</h2>
+          </v-list-item-title>
+          <!-- <v-list-subtitle>
+          Du lịch đó đây
+          </v-list-subtitle> -->
+          <v-img src=""></v-img>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list dense>
+      <v-list-item-group color="primary">
+        <v-list-item style="color: green" v-for="(item, i) in items" :key="i" :to="item.link" link>
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title style="font-size: 16px" v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+    </v-navigation-drawer>
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Những địa điểm du lịch Cần Thơ HẤP DẪN nhất</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <!--  -->
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  export default {
+    data: () => ({ 
+      drawer: null,
+      items: [
+        { title: 'Trang chủ', icon: 'mdi-home', link:"/" },
+        { title: 'Thêm bài viết', icon: 'mdi-note-plus', link:"/add-post" },
+        
+      ],
+    }),
+    
+  }
+</script>
---
+++ src/views/EditPost.vue
@@ -0,0 +1,71 @@
+<template>
+    <v-container>
+        <v-row no-gutters>
+            <v-col sm="10" class="mx-auto">
+                <v-card class="pa-5">
+                    <v-card-title>Sửa bài viết </v-card-title>
+                    <v-divider></v-divider>
+                    <v-form ref="form" @submit.prevent="updateForm" class="pa-5" 
+                    enctype="multipart/form-data">
+                        <v-text-field label="Tiêu đề" v-model="post.title" 
+                        prepend-icon="mdi-note" :rules="rules">
+                        </v-text-field>
+
+                        <v-text-field label="Danh mục" v-model="post.category" 
+                        prepend-icon="mdi-view-list" :rules="rules">
+                        </v-text-field>
+
+                        <v-textarea label="Nội dung" v-model="post.content" 
+                        prepend-icon="mdi-note-plus" :rules="rules"></v-textarea>
+
+                        <v-file-input @change="selectFile" show-size counter multiple
+                        label="Chọn ảnh"></v-file-input>
+                        <v-img :src="`/${post.image}`" width="120"></v-img>
+
+                        <v-btn type="submit" class="mt-3" color="success">Thêm bài viết</v-btn>
+
+                    </v-form>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import API from "../api";
+export default {
+    data() {
+        return {
+            rules: [(value) => !!value || "Trường này là bắt buộc!"],
+            post: {
+                title: "",
+                category: "",
+                content: "",
+                image: ""
+            },
+            image: "",
+        };
+    },
+    async created(){
+        const response = await API.getPostByID(this.$route.params.id);
+        this.post = response;
+    }, 
+    methods: {
+        selectFile(file) {
+            this.image = file[0];
+        },
+        async updateForm() {
+            const formData = new FormData();
+            formData.append("image", this.image);
+            formData.append("title", this.post.title);
+            formData.append("category", this.post.category);
+            formData.append("content", this.post.content);
+            formData.append("old_image", this.post.image);
+            if (this.$refs.form.validate()){
+                const response = await API.updatePost(this.$route.params.id, formData);
+                this.$router.push({ name: "home", params: { message: response.message } });
+            }
+        },
+    },
+};
+</script>
